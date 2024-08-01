646c88c1765bca09a9d25b3bc045d6730bf464ca
Melissa Vela <melissa.vela.coral@gmail.com>
2024-08-01 22:02:18 +1000

    first commit

---
+++ ecommerce-client/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import AuthPage from "./pages/AuthPage";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
